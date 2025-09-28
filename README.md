9420c49e17b9f1123489ea37e4f7c1868c5de5b7
Patrick Terrematte <patrickt@imd.ufrn.br>
2025-09-28 17:59:12 -0300

    Adding files

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # medical-dna-bot
 Medicine AI Bot on Streamlit
+
+## How to run
+
+1. `python -m venv .venv`
+2. `source .venv/bin/activate`
+3. `pip install -r requirements.txt`
+4. `streamlit run main.py`
---
+++ main.py
@@ -0,0 +1,46 @@
+import streamlit as st
+import requests
+class Message:
+    content: str
+    userName: str
+    isChatbot: bool
+
+    def __init__(self, content, userName="Usuário", isChatbot=False):
+        self.content = content
+        self.userName = userName
+        self.isChatbot = isChatbot
+
+def HandleChatbotResponse(response, *args, **kwargs):
+    if response.status_code == 200:
+        print(f"Response received: {response.text}")
+        message = response.text
+        if(message[0] == '"'):
+            message = message[1:-1]
+
+        if(message[-1] == '"'):
+            message = message[:-1]
+        st.session_state.messages.append(Message(message, isChatbot=True, userName="Chatbot"))
+        st.chat_message("assistant").write(message)
+    else:
+        st.error("Erro ao obter resposta do chatbot.")
+
+    st.session_state.waitingResponse = False
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+if "waitingResponse" not in st.session_state:
+    st.session_state.waitingResponse = False
+
+st.title("PCDT Bot")
+
+for message in st.session_state.messages:
+    st.chat_message("assistant" if message.isChatbot else "user").write(message.content)
+
+
+if userPrompt := st.chat_input(disabled=st.session_state.waitingResponse, placeholder="Digite sua mensagem aqui..."):
+    print(f"User prompt: {userPrompt}")
+    st.chat_message("user").write(userPrompt)
+    st.session_state.messages.append(Message(userPrompt, isChatbot=False, userName="Usuário"))
+    st.session_state.waitingResponse = True
+    requests.get("http://localhost:8000/ai", params={"query": userPrompt, "user_id": 1}, hooks={"response": [HandleChatbotResponse]})
